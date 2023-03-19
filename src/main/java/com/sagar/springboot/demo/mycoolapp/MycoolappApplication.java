d053596c514ed1bd94184aa01ffd6e1021461a42
Sagar Gaikwad <gaikwadsagar71@gmail.com>
2023-03-19 13:45:25 +0530

    spring boot actuator

---
+++ src/main/java/com/sagar/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -0,0 +1,14 @@
+package com.sagar.springboot.demo.mycoolapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycoolappApplication {
+
+	public static void main(String[] args) {
+		System.out.println("MycoolappApplication");
+		SpringApplication.run(MycoolappApplication.class, args);
+	}
+
+}
